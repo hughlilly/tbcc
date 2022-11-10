70d8d0e35d4758c8a45fa9e5c206d9b93fe50ebc
Hugh Lilly <hughlilly@users.noreply.github.com>
2022-11-10 17:26:30 +1300

    add eslint & `.prettierrc`

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,22 @@
 /** @type {import('eslint').Linter.Config} */
 module.exports = {
-  extends: ["@remix-run/eslint-config", "@remix-run/eslint-config/node"],
+  extends: [
+    "@remix-run/eslint-config",
+    "@remix-run/eslint-config/node",
+    "eslint:recommended",
+    "prettier",
+  ],
+  plugins: [],
+  parserOptions: {
+    ecmaVersion: 2022,
+    sourceType: "module",
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  env: {
+    es6: true,
+    browser: true,
+    node: true,
+  },
 };
