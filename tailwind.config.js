606cde8681a02ab403b8e4ad40259d59778cd3ac
Hugh Lilly <hughlilly@users.noreply.github.com>
2022-11-10 20:48:52 +1300

    [skip render] tw: custom colour and screen sizes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./app/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./app/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    colors: {
+      purple: "#8e3996",
+    },
+    extend: {
+      screens: {
+        sm: "429px",
+        md: "833px",
+      },
+    },
   },
   plugins: [],
-}
+};
