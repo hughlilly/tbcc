9fcab3b8b6db2fe418121ac9d2602e916bfdff1a
Hugh Lilly <hughlilly@users.noreply.github.com>
2022-11-11 15:55:45 +1300

    add footer + icon

--- app/root.tsx
+++ app/root.tsx
@@ -51,7 +51,7 @@ export function Layout({ children }: any) {
     <>
       <header>
         <nav
-          id="navigation"
+          id="main-navigation"
           className="flex justify-between sm:p-10 px-5 py-8 font-bold"
         >
           {/* See https://tailwindcss.com/docs/line-height#overriding-default-line-heights */}
@@ -63,7 +63,7 @@ export function Layout({ children }: any) {
             {siteTitle}
           </Link>
           <div
-            id="navlinks"
+            id="main-navlinks"
             className="hidden sm:flex flex-row items-center gap-5 text-purple text-sm"
           >
             <NavLink
@@ -99,8 +99,74 @@ export function Layout({ children }: any) {
           </div>
         </nav>
       </header>
-      {children}
-      <footer>&copy; 2022 Telugu Badminton Club of Canterbury</footer>
+
+      <div className="">{children}</div>
+
+      <footer className="fixed bottom-0 left-0 w-full grid items-center p-10 md:px-40 lg:px-80 md:py-10 lg:py-20 bg-purple min-h-[200px]">
+        <nav
+          id="footer-navigation"
+          className="flex flex-col sm:flex-row gap-y-12 sm:gap-y-0 justify-between text-white"
+        >
+          <div className="flex items-center justify-between">
+            {/* See https://tailwindcss.com/docs/line-height#overriding-default-line-heights */}
+            <span
+              id="wordmark"
+              className="text-footerWordmark text-xl leading-5 sm:text-[1.15rem] sm:leading-5 font-bold cursor-default sm:grid self-center sm:self-end w-[10ch]"
+            >
+              {siteTitle}
+            </span>
+            <div id="footer-icon" className="sm:hidden">
+              <img
+                src={"icons/badminton.png"}
+                height={"40px"}
+                width={"40px"}
+                alt={"Badminton icon"}
+              />
+            </div>
+          </div>
+          <div
+            id="footer-navlinks"
+            className="flex flex-row justify-between self-auto sm:gap-x-10 text-sitemapLink text-sm font-[500]"
+          >
+            <div
+              id="footer-navlinks-main"
+              className="inline-flex flex-col self-end"
+            >
+              <NavLink className="hover:text-white" to="/about">
+                About
+              </NavLink>
+              <NavLink className="hover:text-white" to="/tournaments">
+                Tournaments
+              </NavLink>
+              <NavLink className="hover:text-white" to="/laws">
+                Badminton Laws
+              </NavLink>
+              <NavLink className="hover:text-white" to="/training">
+                Training Materials
+              </NavLink>
+            </div>
+            <div
+              id="footer-navlinks-extra"
+              className="inline-flex flex-col self-end"
+            >
+              <NavLink className="hover:text-white" to="/contact">
+                Contact
+              </NavLink>
+              <NavLink className="hover:text-white" to="/privacy-policy">
+                Privacy Policy
+              </NavLink>
+            </div>
+          </div>
+          <div id="footer-icon" className="hidden sm:grid self-center">
+            <img
+              src={"icons/badminton.png"}
+              height={"40px"}
+              width={"40px"}
+              alt={"Badminton icon"}
+            />
+          </div>
+        </nav>
+      </footer>
     </>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,12 @@
 module.exports = {
   content: ["./app/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    colors: {
-      purple: "#8e3996",
-    },
     extend: {
+      colors: {
+        purple: "#8e3996",
+        sitemapLink: "#F0F0F0",
+        footerWordmark: "#E1E1E1",
+      },
       screens: {
         sm: "429px",
         md: "833px",
