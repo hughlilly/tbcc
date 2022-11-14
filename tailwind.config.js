941b6e57575959d5f8c02cd710a208f2cb0249fa
Hugh Lilly <hughlilly@users.noreply.github.com>
2022-11-14 22:10:41 +1300

    [skip render] format tw config
    also set hero bg image var

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./app/**/*.{js,ts,jsx,tsx}"],
+  content: ['./app/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
       colors: {
-        purple: "#8e3996",
-        sitemapLink: "#F0F0F0",
-        footerWordmark: "#E1E1E1",
-        heroText: "#E1E1E1",
+        purple: '#8e3996',
+        sitemapLink: '#F0F0F0',
+        footerWordmark: '#E1E1E1',
+        heroText: '#E1E1E1',
       },
       screens: {
-        sm: "429px",
-        md: "833px",
+        sm: '429px',
+        md: '833px',
+      },
+      backgroundImage: {
+        hero: "url('https://res.cloudinary.com/tbcc-media/image/upload/v1668049222/aboodi_vesakaran_nu_D_Bpg_KO_2_Pc_unsplash_dadba394fa.jpg')",
       },
     },
   },
