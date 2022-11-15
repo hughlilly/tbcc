0e7e712bb0a4f8872628a656d0627c139a2d6cd1
Hugh Lilly <hughlilly@users.noreply.github.com>
2022-11-15 16:06:42 +1300

    add mobile menu

--- app/shared/components/header.tsx
+++ app/shared/components/header.tsx
@@ -1,57 +1,99 @@
 import { Link, NavLink } from '@remix-run/react';
+import { useState } from 'react';
 import { siteTitle } from '~/root';
 
 export default function Header() {
+  const [isNavOpen, setIsNavOpen] = useState(false);
+
   return (
-    <header className="flex h-24 w-full sm:h-32">
-      <nav
-        id="main-navigation"
-        className="flex w-full flex-row items-center justify-between p-5 font-bold md:px-16 lg:px-24 "
+    <header className="flex flex-col">
+      <div id="menu-top" className="flex h-20 w-full flex-col sm:h-32 sm:flex-row">
+        <nav
+          id="main-navigation"
+          className="flex w-full items-center justify-between p-6 font-bold md:px-16 lg:px-24"
+        >
+          {/* See https://tailwindcss.com/docs/line-height#overriding-default-line-heights */}
+          <Link
+            id="wordmark"
+            to="/"
+            className="w-[14ch] text-sm leading-4 sm:text-lg sm:leading-5 sm:max-md:w-[10ch]"
+          >
+            {siteTitle}
+          </Link>
+          <section
+            id="main-navlinks"
+            className="hidden flex-row justify-between gap-x-5 text-sm text-purple sm:flex"
+          >
+            <NavLink
+              to="/about"
+              className="hover:underline hover:decoration-2 hover:underline-offset-4"
+            >
+              About
+            </NavLink>
+            <NavLink
+              to="/tournaments"
+              className="hover:underline hover:decoration-2 hover:underline-offset-4"
+            >
+              Tournaments
+            </NavLink>
+            <NavLink
+              to="/laws"
+              className="hover:underline hover:decoration-2 hover:underline-offset-4"
+            >
+              Badminton Laws
+            </NavLink>
+            <NavLink
+              to="/training"
+              className="hover:underline hover:decoration-2 hover:underline-offset-4 sm:max-md:hidden"
+            >
+              Training Materials
+            </NavLink>
+            <NavLink
+              to="/contact"
+              className="hover:underline hover:decoration-2 hover:underline-offset-4"
+            >
+              Contact
+            </NavLink>
+          </section>
+          <div
+            id="hamburger-icon"
+            className={`${isNavOpen ? 'hidden' : 'block'} space-y-1 pr-1 sm:hidden`}
+            onClick={() => setIsNavOpen((state) => !state)}
+          >
+            <span className="block h-0.5 w-5 bg-neutral-800"></span>
+            <span className="block h-0.5 w-5 bg-neutral-800"></span>
+            <span className="block h-0.5 w-5 bg-neutral-800"></span>
+          </div>
+          <div
+            id="cross-icon"
+            className={`${isNavOpen ? 'flex' : 'hidden'} sm:hidden`}
+            onClick={() => setIsNavOpen(false)}
+          >
+            <svg
+              className="h-8 w-7 fill-neutral-800"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            >
+              <line x1="18" y1="6" x2="6" y2="18" />
+              <line x1="6" y1="6" x2="18" y2="18" />
+            </svg>
+          </div>
+        </nav>
+      </div>
+      <section
+        id="mobile-menu"
+        className={`h-[calc(100vh-5rem)] flex-col items-center justify-around border-t-[1px] border-t-neutral-300 p-10 py-20 text-sm font-bold text-purple sm:hidden 
+          ${isNavOpen ? 'flex' : 'hidden'} `}
       >
-        {/* See https://tailwindcss.com/docs/line-height#overriding-default-line-heights */}
-        <Link
-          id="wordmark"
-          to="/"
-          className="w-[14ch] text-sm leading-4 sm:text-lg  sm:leading-5 sm:max-md:w-[10ch]"
-        >
-          {siteTitle}
-        </Link>
-        <div
-          id="main-navlinks"
-          className="hidden flex-row justify-between gap-x-5 text-sm text-purple sm:flex"
-        >
-          <NavLink
-            to="/about"
-            className="hover:underline hover:decoration-2 hover:underline-offset-4"
-          >
-            About
-          </NavLink>
-          <NavLink
-            to="/tournaments"
-            className="hover:underline hover:decoration-2 hover:underline-offset-4"
-          >
-            Tournaments
-          </NavLink>
-          <NavLink
-            to="/laws"
-            className="hover:underline hover:decoration-2 hover:underline-offset-4"
-          >
-            Badminton Laws
-          </NavLink>
-          <NavLink
-            to="/training"
-            className="hover:underline hover:decoration-2 hover:underline-offset-4 sm:max-md:hidden"
-          >
-            Training Materials
-          </NavLink>
-          <NavLink
-            to="/contact"
-            className="hover:underline hover:decoration-2 hover:underline-offset-4"
-          >
-            Contact
-          </NavLink>
-        </div>
-      </nav>
+        <NavLink to="/about">About</NavLink>
+        <NavLink to="/tournaments">Tournaments</NavLink>
+        <NavLink to="/laws">Badminton Laws</NavLink>
+        <NavLink to="/training">Training Materials</NavLink>
+        <NavLink to="/contact">Contact</NavLink>
+      </section>
     </header>
   );
 }
--- app/shared/components/hero.tsx
+++ app/shared/components/hero.tsx
@@ -2,7 +2,7 @@ import { siteTitle } from '~/root';
 
 export default function Hero() {
   return (
-    <div className="relative h-[calc(100vh-6rem)] bg-hero bg-cover bg-center bg-no-repeat sm:h-[calc(100vh-8rem)]">
+    <div className="relative h-[calc(100vh-5rem)] bg-hero bg-cover bg-center bg-no-repeat sm:h-[calc(100vh-8rem)]">
       <span className="absolute right-8 bottom-8 w-[8ch] text-right text-xl font-bold leading-5 text-heroText sm:w-[14ch] sm:text-4xl xl:text-5xl">
         {siteTitle}
       </span>
