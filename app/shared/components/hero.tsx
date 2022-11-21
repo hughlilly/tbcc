9a50edaf8998e9b650fa0201c061cae29d4a874e
Hugh Lilly <hughlilly@users.noreply.github.com>
2022-11-21 18:29:12 +1300

    fix bg-hero string

--- app/shared/components/hero.tsx
+++ app/shared/components/hero.tsx
@@ -5,10 +5,10 @@ export default function Hero({
   page: string;
   text: string;
 }) {
+  const pageString = `bg-hero-${page}`;
   return (
     <div
-      // eslint-disable-next-line tailwindcss/no-custom-classname
-      className={`bg-hero-${page} relative h-[calc(100vh-5rem)] bg-cover bg-center bg-no-repeat sm:h-[calc(100vh-8rem)]`}
+      className={`${pageString} relative h-[calc(100vh-5rem)] bg-hero-home bg-cover bg-center bg-no-repeat sm:h-[calc(100vh-8rem)]`}
     >
       <span className="absolute right-8 bottom-8 w-[8ch] text-right text-xl font-bold leading-5 text-heroText sm:w-[14ch] sm:text-4xl xl:text-5xl">
         {text}
