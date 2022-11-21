373ee271b4ca655a306443617d96c264b3bf67c5
Hugh Lilly <hughlilly@users.noreply.github.com>
2022-11-21 18:36:56 +1300

    Hero component workaround for dynamic content

--- app/shared/components/hero.tsx
+++ app/shared/components/hero.tsx
@@ -5,10 +5,24 @@ export default function Hero({
   page: string;
   text: string;
 }) {
-  const pageString = `bg-hero-${page}`;
+  const baseString = ` relative h-[calc(100vh-5rem)] bg-cover bg-center bg-no-repeat sm:h-[calc(100vh-8rem)]`;
   return (
     <div
-      className={`${pageString} relative h-[calc(100vh-5rem)] bg-hero-home bg-cover bg-center bg-no-repeat sm:h-[calc(100vh-8rem)]`}
+      className={
+        page === "home"
+          ? "bg-hero-home" + baseString
+          : page === "about"
+          ? "bg-hero-about" + baseString
+          : page === "faq"
+          ? "bg-hero-faq" + baseString
+          : page === "tournaments"
+          ? "bg-hero-tournaments" + baseString
+          : page === "laws"
+          ? "bg-hero-laws" + baseString
+          : page === "training"
+          ? "bg-hero-training" + baseString
+          : ""
+      }
     >
       <span className="absolute right-8 bottom-8 w-[8ch] text-right text-xl font-bold leading-5 text-heroText sm:w-[14ch] sm:text-4xl xl:text-5xl">
         {text}
