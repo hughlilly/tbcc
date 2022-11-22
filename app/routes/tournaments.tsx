83ca06b45685b5b2d2f7b6998f9329a1efc0d197
Hugh Lilly <hughlilly@users.noreply.github.com>
2022-11-23 11:53:41 +1300

    [skip render] truncate descriptions w js fn

--- app/routes/tournaments.tsx
+++ app/routes/tournaments.tsx
@@ -1,5 +1,13 @@
 import { Outlet } from "@remix-run/react";
+import Hero from "~/shared/components/hero";
+
+export const tournamentsSectionName = "Tournaments";
 
 export default function TournamentsRoute() {
-  return <Outlet />;
+  return (
+    <div id="laws-page-content">
+      <Hero text={`${tournamentsSectionName}`} page="tournaments" />
+      <Outlet />
+    </div>
+  );
 }
--- app/routes/tournaments/index.tsx
+++ app/routes/tournaments/index.tsx
@@ -35,26 +35,119 @@ export async function loader() {
     });
   }
 
+  for (const tournament of data.data) {
+    for (const tournamentPhoto of tournament.attributes.photos.data) {
+      if (
+        tournamentPhoto.attributes.formats.medium &&
+        !tournamentPhoto.attributes.formats.medium.url.startsWith(
+          "http"
+        )
+      ) {
+        tournamentPhoto.attributes.formats.medium.url =
+          process.env.STRAPI_URL_BASE +
+          tournamentPhoto.attributes.formats.medium.url;
+      }
+    }
+  }
+
   return data.data;
 }
 
 export default function Tournaments() {
   const tournaments = useLoaderData();
 
+  // https://stackoverflow.com/a/57688223/10267529
+  const truncateString = (string = "", maxLength = 50) =>
+    string.length > maxLength
+      ? `${string.substring(0, maxLength)}`
+      : string;
+
   return (
-    <ul>
-      {tournaments.map((tournament: any) => (
-        <li key={tournament.id}>
-          <Link to={tournament.attributes.slug}>
-            {tournament.attributes.title}
-          </Link>
-          <span className="italic">
-            ({tournament.attributes.location.data.attributes.name},{" "}
-            {tournament.attributes.location.data.attributes.address})
-          </span>
-          <div className="text-sm">{tournament.attributes.desc}</div>
-        </li>
-      ))}
-    </ul>
+    <div
+      id="tournaments-info"
+      className="flex min-h-[50vh] flex-col justify-evenly gap-x-10 gap-y-14 p-5 pt-20 pb-14 sm:py-20 md:px-24 lg:flex-row lg:max-xl:px-44 lg:max-lg:px-96"
+    >
+      {tournaments.map((tournament: any) => {
+        const tournamentData = tournament.attributes;
+        const locationData =
+          tournament.attributes.location.data.attributes;
+        const photoData =
+          tournament.attributes.photos.data[0].attributes;
+        return (
+          <div
+            className="flex flex-col gap-y-5 sm:px-20"
+            key={tournament.id}
+          >
+            <Link
+              to={tournamentData.slug}
+              className="flex flex-col items-center gap-y-5"
+            >
+              <img
+                src={
+                  process.env.NODE_ENV === "development"
+                    ? photoData.formats.medium.url
+                    : photoData.url
+                }
+                alt={
+                  tournamentData.photos.data[0].attributes
+                    .alternativeText
+                }
+                className="h-48 w-48 rounded-full"
+              />
+              <h1 className="pt-8 text-center text-2xl font-bold sm:gap-y-0 sm:py-0">
+                {tournamentData.title}
+              </h1>
+            </Link>
+            <div
+              id={`${tournamentData.slug}-metadata`}
+              className="text-sm"
+            >
+              <p className="pt-5 font-bold">{locationData.name}</p>
+              <p>
+                {new Date(tournamentData.startTime).toLocaleDateString(
+                  undefined,
+                  {
+                    dateStyle: "full",
+                  }
+                )}
+              </p>
+              <p>
+                <time
+                  dateTime={tournamentData.startTime}
+                  id={`${tournamentData.slug}-start-time`}
+                >
+                  {new Date(
+                    tournamentData.startTime
+                  ).toLocaleTimeString(undefined, {
+                    timeStyle: "short",
+                  })}
+                </time>
+                <span> – </span>
+                <time
+                  dateTime={tournamentData.endTime}
+                  id={`${tournamentData.slug}-end-time`}
+                >
+                  {new Date(tournamentData.endTime).toLocaleTimeString(
+                    undefined,
+                    {
+                      timeStyle: "short",
+                    }
+                  )}
+                </time>
+              </p>
+            </div>
+            <p>
+              {truncateString(tournamentData.desc, 220)}{" "}
+              <Link
+                to={tournamentData.slug}
+                className="font-bold hover:underline"
+              >
+                Read more…
+              </Link>
+            </p>
+          </div>
+        );
+      })}
+    </div>
   );
 }
