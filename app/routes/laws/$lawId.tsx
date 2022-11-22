5ef4c4ee1ada960029a36f8865a934d949914546
Hugh Lilly <hughlilly@users.noreply.github.com>
2022-11-22 21:50:49 +1300

    add laws files

---
+++ app/routes/laws/$lawId.tsx
@@ -0,0 +1,134 @@
+import { NavLink, useLoaderData } from "@remix-run/react";
+import { checkStatus, checkEnvVars } from "~/utils/errorHandling";
+import { siteTitle } from "~/root";
+import Hero from "~/shared/components/hero";
+import { lawsSectionName } from "./index";
+
+export function meta({ data }: any): { title: string } {
+  return {
+    title: `${data.attributes.LawName} | ${lawsSectionName} | ${siteTitle}`,
+  };
+}
+
+export async function loader({ params }: any) {
+  checkEnvVars();
+
+  const res = await fetch(
+    `${process.env.STRAPI_URL_BASE}/api/laws` +
+      `?populate=*&filters[slug]=${params.trainingId}`,
+    {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${process.env.STRAPI_API_TOKEN}`,
+        "Content-Type": "application/json",
+      },
+    }
+  );
+
+  // Handle HTTP response code < 200 or >= 300
+  checkStatus(res);
+
+  const data = await res.json();
+
+  // Did Strapi return an error object in its response?
+  if (data.error) {
+    console.error("Error", data.error);
+    throw new Response("Error getting data from Strapi", {
+      status: 500,
+    });
+  }
+
+  // Did Strapi return an empty list?
+  if (!data.data || data.data.length === 0) {
+    console.error(data);
+    throw new Response("Not Found", { status: 404 });
+  }
+
+  const shot = data.data[0];
+
+  for (const photo of shot.attributes.Photo.data) {
+    if (
+      photo.attributes.formats.thumbnail &&
+      !photo.attributes.formats.thumbnail.url.startsWith("http")
+    ) {
+      photo.attributes.formats.thumbnail =
+        process.env.STRAPI_URL_BASE +
+        photo.attributes.formats.thumbnail;
+    }
+
+    if (
+      photo.attributes.formats.small &&
+      !photo.attributes.formats.small.url.startsWith("http")
+    ) {
+      photo.attributes.formats.small.url =
+        process.env.STRAPI_URL_BASE +
+        photo.attributes.formats.small.url;
+    }
+
+    if (
+      photo.attributes.formats.medium &&
+      !photo.attributes.formats.medium.url.startsWith("http")
+    ) {
+      photo.attributes.formats.medium.url =
+        process.env.STRAPI_URL_BASE +
+        photo.attributes.formats.medium.url;
+    }
+
+    if (
+      photo.attributes.formats.large &&
+      !photo.attributes.formats.large.url.startsWith("http")
+    ) {
+      photo.attributes.formats.large.url =
+        process.env.STRAPI_URL_BASE +
+        photo.attributes.formats.large.url;
+    }
+  }
+  return shot;
+}
+
+export default function LawsRoute() {
+  const shot = useLoaderData();
+
+  return (
+    <div className="flex flex-col">
+      <Hero
+        text={`${lawsSectionName}: ${shot.attributes.ShotName}`}
+        page="training"
+      />
+      <div
+        className="flex flex-col py-10 sm:flex-row sm:py-0"
+        key={shot.id}
+      >
+        <div
+          // See https://stackoverflow.com/a/70805360/10267529
+          style={{
+            backgroundImage: `url(${
+              process.env.NODE_ENV === "development"
+                ? shot.attributes.Photo.data[0].attributes.formats
+                    .medium.url
+                : shot.attributes.Photo.data[0].attributes.url
+            })`,
+          }}
+          role="img"
+          // See https://www.davidmacd.com/blog/alternate-text-for-css-background-images.html
+          aria-label={`${shot.attributes.Photo.data[0].attributes.alternativeText}`}
+          className="mx-auto h-44 w-44 rounded-full bg-cover bg-center bg-no-repeat sm:h-[55vh] sm:min-w-[50%] sm:rounded-none lg:min-w-[50%]"
+        />
+        <div className="flex flex-col justify-between sm:w-1/2 sm:p-10">
+          <div className="flex flex-col gap-y-5">
+            <h1 className="pt-8 text-center text-2xl font-bold sm:gap-y-0 sm:py-0">
+              {shot.attributes.ShotName}
+            </h1>
+            <p>{shot.attributes.Description}</p>
+          </div>
+          <NavLink
+            to="../"
+            className="text-center font-bold text-purple hover:underline hover:decoration-2 hover:underline-offset-4"
+          >
+            ← Back to {lawsSectionName}
+          </NavLink>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ app/routes/laws/index.tsx
@@ -0,0 +1,93 @@
+import { Link, useLoaderData } from "@remix-run/react";
+import { siteTitle } from "~/root";
+import Hero from "~/shared/components/hero";
+import { checkEnvVars, checkStatus } from "~/utils/errorHandling";
+
+export const lawsSectionName = `Badminton Laws`;
+
+export function meta() {
+  return {
+    title: `${lawsSectionName} | ${siteTitle}`,
+  };
+}
+
+export async function loader() {
+  checkEnvVars();
+
+  const res = await fetch(
+    `${process.env.STRAPI_URL_BASE}/api/laws/?populate=*`,
+    {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${process.env.STRAPI_API_TOKEN}`,
+        "Content-Type": "application/json",
+      },
+    }
+  );
+
+  // Handle HTTP response code < 200 or >= 300
+  checkStatus(res);
+
+  const data = await res.json();
+
+  // Did Strapi return an error object in its response?
+  if (data.error) {
+    console.error("Error", data.error);
+    throw new Response("Error getting data from Strapi", {
+      status: 500,
+    });
+  }
+
+  if (
+    data.data[0].attributes.Photo.data.attributes.formats.medium.url &&
+    !data.data[0].attributes.Photo.data.attributes.formats.medium.url.startsWith(
+      "http"
+    )
+  ) {
+    data.data[0].attributes.Photo.data.attributes.formats.medium.url =
+      process.env.STRAPI_URL_BASE +
+      data.data[0].attributes.Photo.data.attributes.formats.medium.url;
+  }
+
+  return data.data;
+}
+
+export default function LawsRoute() {
+  const data = useLoaderData();
+
+  return (
+    <div id="laws-page-content">
+      <Hero text={`${lawsSectionName}`} page="laws" />
+      <div
+        id="laws-info"
+        className="flex min-h-[50vh] flex-col justify-evenly gap-x-10 gap-y-14 p-5 pt-20 pb-14 sm:py-20 md:px-24 lg:flex-row lg:max-xl:px-44 lg:max-lg:px-96"
+      >
+        {data.map((law: any) => (
+          <div className="flex flex-col gap-y-5" key={law.id}>
+            <Link
+              to={law.attributes.slug}
+              className="flex flex-col items-center gap-y-5"
+            >
+              <img
+                src={
+                  process.env.NODE_ENV === "development"
+                    ? law.attributes.Photo.data.attributes.formats
+                        .medium.url
+                    : law.attributes.Photo.data.attributes.url
+                }
+                alt={
+                  law.attributes.Photo.data.attributes.alternativeText
+                }
+                className="h-48 w-48 rounded-full"
+              />
+              <h1 className="pt-8 text-center text-2xl font-bold sm:gap-y-0 sm:py-0">
+                {law.attributes.LawName}
+              </h1>
+            </Link>
+            <p>{law.attributes.Description}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
