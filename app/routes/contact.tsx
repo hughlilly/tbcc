c4c92eada8969a645d7cdbf2b2b1bd4eb7a11a93
Hugh Lilly <hughlilly@users.noreply.github.com>
2022-11-25 13:13:24 +1300

    Put Components back in file

--- app/routes/contact.tsx
+++ app/routes/contact.tsx
@@ -1,11 +1,33 @@
-import ContactForm from "~/shared/components/contact-form";
 import { siteTitle } from "~/root";
+import type { ActionFunction } from "@remix-run/node";
+import { checkEnvVars, checkStatus } from "~/utils/errorHandling";
+import { useActionData, Form } from "@remix-run/react";
 
 export function meta() {
   return {
     title: `Contact Us | ${siteTitle}`,
   };
 }
+
+export let action: ActionFunction = async ({ request }) => {
+  let formData = await request.formData();
+  if (process.env.NODE_ENV === "development") {
+    checkEnvVars();
+
+    const res = await fetch(`${process.env.STRAPI_URL_BASE}/messages`, {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${process.env.STRAPI_API_TOKEN}`,
+        "Content-Type": "application/json",
+      },
+    });
+
+    // Handle HTTP response code < 200 or >= 300
+    checkStatus(res);
+
+    return res.json();
+  }
+};
 
 export default function ContactRoute() {
   return (
@@ -16,29 +38,148 @@ export default function ContactRoute() {
       className="flex flex-col divide-y divide-solid lg:flex-row lg:divide-none"
     >
       <ContactForm />
-      <div
-        id="map-content"
-        className="flex flex-col justify-center pt-10 lg:mx-auto lg:pt-0"
+      <Map />
+    </div>
+  );
+}
+
+function ContactForm() {
+  const actionData = useActionData();
+
+  return (
+    <div id="form-content" className="mx-auto px-4 pt-5 pb-10 text-sm">
+      <Form
+        method="post"
+        autoComplete="on"
+        aria-describedby="contact-form-desc"
+        className="flex flex-col gap-y-4"
       >
-        <div
-          id="header-address-block"
-          className="mx-auto flex flex-row items-center gap-x-5 px-10 pb-5 sm:gap-x-10 "
-        >
-          <div id="find-us" className="text-2xl font-bold">
-            Find Us
+        <fieldset className="flex flex-col gap-y-4">
+          <legend className="mb-5 text-center text-2xl font-bold">
+            Contact Us
+          </legend>
+          <p
+            id="contact-form-desc"
+            className="mb-5 text-center md:text-base"
+          >
+            Use this form to contact us to ask about membership.
+          </p>
+          <div id="firstname-field">
+            <label htmlFor="firstname" className="mb-1 block">
+              First name (required)
+            </label>
+            <input
+              required
+              className="block w-full appearance-none border border-gray-200 py-3 px-4"
+              id="firstname"
+              name="firstname"
+              type="text"
+              placeholder="First name" // See https://developer.mozilla.org/en-US/docs/Web/HTML/Attributes/autocomplete#values
+              // and https://www.w3.org/TR/WCAG21/#input-purposes
+              autoComplete="given-name"
+            />
           </div>
-          <div id="address" className="text-sm">
-            <p>220B Pages Road</p>
-            <p>Wainoni</p>
-            <p>Christchurch 8062</p>
+          {/* <p className="text-xs italic text-red-500">Please fill out this field.</p> */}
+          <div id="lastname-field">
+            <label htmlFor="lastname" className="mb-1 block">
+              Last name (required)
+            </label>
+            <input
+              required
+              className="block w-full appearance-none border border-gray-200 py-3 px-4"
+              id="lastname"
+              name="lastname"
+              type="text"
+              placeholder="Last name"
+              autoComplete="family-name"
+            />
           </div>
+          <div id="phone-number-field">
+            <label htmlFor="phoneNumber" className="mb-1 block">
+              Phone number
+            </label>
+            <input
+              className="block w-full appearance-none border border-gray-200 py-3 px-4"
+              id="phoneNumber"
+              name="phoneNumber"
+              type="tel"
+              placeholder="Phone number"
+            />
+          </div>
+          <div id="email-field">
+            <label htmlFor="email" className="mb-1 block">
+              Email address
+            </label>
+            <input
+              className="block w-full appearance-none border border-gray-200 py-3 px-4"
+              id="email"
+              name="email"
+              type="email"
+              placeholder="Email address"
+            />
+          </div>
+          <div id="message-field">
+            <label htmlFor="message" className="mb-1 block">
+              Message
+            </label>
+            <textarea
+              autoComplete="off"
+              rows={8}
+              className="block w-full appearance-none border border-gray-200 py-3 px-4"
+              id="message"
+              name="message"
+              placeholder="Message"
+            />
+          </div>
+          <div
+            id="newsletter-tickbox"
+            className="mx-auto flex flex-row items-center gap-x-4"
+          >
+            <input type="checkbox" name="newsletter" id="newsletter" />
+            <label
+              htmlFor="newsletter"
+              className="select-none hover:cursor-pointer"
+            >
+              Yes, sign me up to the newsletter
+            </label>
+          </div>
+        </fieldset>
+        <input
+          type="submit"
+          name="Submit"
+          value="Submit"
+          className="mx-auto bg-purple py-3 px-2 text-white hover:cursor-pointer"
+        />
+      </Form>
+      <p>{actionData?.error ? actionData.message : <>&nbsp;</>} </p>
+    </div>
+  );
+}
+
+function Map() {
+  return (
+    <div
+      id="map-content"
+      className="flex flex-col justify-center pt-10 lg:mx-auto lg:pt-0"
+    >
+      <div
+        id="header-address-block"
+        className="mx-auto flex flex-row items-center gap-x-5 px-10 pb-5 sm:gap-x-10 "
+      >
+        <div id="find-us" className="text-2xl font-bold">
+          Find Us
         </div>
-        <iframe
-          className="lg:w- min-h-[50vh] lg:h-[32rem] lg:w-[32rem]"
-          title="Telugu Badminton Club on Google Maps"
-          src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d46289.24428014558!2d172.695569!3d-43.521571!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x974a13458cf5afe!2sTelugu%20Badminton%20Club!5e0!3m2!1sen!2sus!4v1668976283083!5m2!1sen!2sus"
-        ></iframe>
+        <div id="address" className="text-sm">
+          <p>220B Pages Road</p>
+          <p>Wainoni</p>
+          <p>Christchurch 8062</p>
+        </div>
       </div>
+      <iframe
+        className="lg:w- min-h-[50vh] lg:h-[32rem] lg:w-[32rem]"
+        title="Telugu Badminton Club on Google Maps"
+        src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d46289.24428014558!2d172.695569!3d-43.521571!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x974a13458cf5afe!2sTelugu%20Badminton%20Club!5e0!3m2!1sen!2sus!4v1668976283083!5m2!1sen!2sus"
+      ></iframe>
     </div>
   );
 }
